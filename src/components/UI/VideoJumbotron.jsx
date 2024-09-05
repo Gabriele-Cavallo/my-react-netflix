c3def1f60088da31223089809df4230f5d5b7f1d
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-05 21:53:49 +0200

    feat: add trailer.js file

--- src/components/UI/VideoJumbotron.jsx
+++ src/components/UI/VideoJumbotron.jsx
@@ -1,24 +1,13 @@
 import { useEffect, useState } from "react";
-
-const trailerKeys = [
-    'qhAB4Y1VrO8',
-    'pgW_c_tPork',
-    'jFBeY-xr30Y',
-    'Yn9KdDHqSyY',
-    'YpSDc25aJSo',
-    'oZqzxhQpqL4',
-    'vYsCcosc1p4',
-    'QS8Kk39ky5Q',
-    'pPLpb-boSoE'
-];
-
+import { Link } from "react-router-dom";
+import { trailers } from "../../util/trailers";
 
 export default function VideoJumbotron(){
-    const randomTrailer = trailerKeys[Math.floor(Math.random() * trailerKeys.length)];
-    const [iframeSrc, setIframeSrc] = useState(`https://www.youtube.com/embed/${randomTrailer}?autoplay=1&mute=1`);
+    const randomTrailer = trailers[Math.floor(Math.random() * trailers.length)];
+    const [iframeSrc, setIframeSrc] = useState(`https://www.youtube.com/embed/${randomTrailer.key}?autoplay=1&mute=1`);
     
     useEffect(() => {
-        setIframeSrc(`https://www.youtube.com/embed/${randomTrailer}?autoplay=1&mute=0`)
+        setIframeSrc(`https://www.youtube.com/embed/${randomTrailer.key}?autoplay=1&mute=0`)
     }, [randomTrailer])
 
     return (
@@ -39,6 +28,18 @@ export default function VideoJumbotron(){
                     objectFit: "cover",
                 }}
             />
+            <div className="absolute left-[10vw] top-[45%] translate-y-[-50%]">
+                <h2 className="font-bold text-3xl mb-3">{randomTrailer.title}</h2>
+                <p className="w-[25vw]">{randomTrailer.description}</p>
+                <Link to={`/movie/${randomTrailer.id}`}>
+                    <button className="p-4 mt-3 rounded-xl bg-white/20 flex gap-3 align-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                        </svg>
+                        <span className="text-xl">Altre Info</span>
+                    </button>
+                </Link>
+            </div>
             <div className="absolute z-10 left-0 bottom-0 h-[283px] inset-x-0 bg-[#141414]"></div>
         </div>
     )
---
+++ src/util/trailers.js
@@ -0,0 +1,56 @@
+export const trailers = [
+    {
+        id: 533535,
+        key: 'qhAB4Y1VrO8',
+        title: 'Deadpool & Wolverine',
+        description: 'A listless Wade Wilson toils away in civilian life with his days as the morally flexible mercenary, Deadpool, behind him. But when his homeworld faces an existential threat, Wade must reluctantly suit-up again with an even more reluctant Wolverine.',
+    },
+    {
+        id: 561,
+        key: 'pgW_c_tPork',
+        title: 'Constatine 2',
+        description: 'John Constantine has literally been to Hell and back. When he teams up with a policewoman to solve the mysterious suicide of her twin sister, their investigation takes them through the world of demons and angels that exists beneath the landscape of contemporary Los Angeles.',
+    },
+    {
+        id: 945961,
+        key: 'jFBeY-xr30Y',
+        title: 'Alien: Romulus',
+        description: 'While scavenging the deep ends of a derelict space station, a group of young space colonizers come face to face with the most terrifying life form in the universe.',
+    },
+    {
+        id: 1022789,
+        key: 'Yn9KdDHqSyY',
+        title: 'Inside Out 2',
+        description: `Teenager Riley's mind headquarters is undergoing a sudden demolition to make room for something entirely unexpected: new Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been running a successful operation by all accounts, aren’t sure how to feel when Anxiety shows up. And it looks like she’s not alone.`,
+    },
+    {
+        id: 653346,
+        key: 'YpSDc25aJSo',
+        title: 'Il regno del pianeta delle scimmie',
+        description: 'Several generations following Caesar\'s reign, apes – now the dominant species – live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all he\'s known about the past and to make choices that will define a future for apes and humans alike.',
+    },
+    {
+        id: 1111873,
+        key: 'oZqzxhQpqL4',
+        title: 'Abigail',
+        description: 'A group of criminals kidnaps a teenage ballet dancer, the daughter of a notorious gang leader, in order to obtain a ransom of $50 million, but over time, they discover that she is not just an ordinary girl. After the kidnappers begin to diminish, one by one, they discover, to their increasing horror, that they are locked inside with an unusual girl.',
+    },
+    {
+        id: 718821,
+        key: 'vYsCcosc1p4',
+        title: 'Twisters',
+        description: 'As storm season intensifies, the paths of former storm chaser Kate Carter and reckless social-media superstar Tyler Owens collide when terrifying phenomena never seen before are unleashed. The pair and their competing teams find themselves squarely in the paths of multiple storm systems converging over central Oklahoma in the fight of their lives.',
+    },
+    {
+        id: 967847,
+        key: 'QS8Kk39ky5Q',
+        title: 'Ghostbusters: Frozen Empire',
+        description: 'When the discovery of an ancient artifact unleashes an evil force, Ghostbusters new and old must join forces to protect their home and save the world from a second Ice Age.',
+    },
+    {
+        id: 786892,
+        key: 'pPLpb-boSoE',
+        title: 'Furiosa: A Mad Max Saga',
+        description: 'As the world fell, young Furiosa is snatched from the Green Place of Many Mothers and falls into the hands of a great Biker Horde led by the Warlord Dementus. Sweeping through the Wasteland they come across the Citadel presided over by The Immortan Joe. While the two Tyrants war for dominance, Furiosa must survive many trials as she puts together the means to find her way home.',
+    },
+];
